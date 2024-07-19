3d131650a8564e97df4d9e6b47c88781c3754c49
sachin3586 <sachindadhe55@gmail.com>
2024-07-19 19:09:19 +0530

    commit

--- src/main/java/util/DropDownUtil.java
+++ src/main/java/util/DropDownUtil.java
@@ -1,4 +1,28 @@
 package util;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
 public class DropDownUtil {
+    public static Select select;
+
+    public static void bootstrapDropdown(List<WebElement> elementList, String option){
+        for (WebElement element:elementList){
+            if (element.getText().equalsIgnoreCase(option)){
+                element.click();
+                break;
+            }
+
+        }
+    }
+
+    public static void selectOptionByVisibleText(WebElement element,String option){
+        select=new Select(element);
+        select.selectByVisibleText(option);
+
+    }
+
+
 }
